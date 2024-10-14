65b12a54af17f946b25bdb16c1cd73d8cce49f58
barman <barmansystemsp@gmail.com>
2024-10-14 16:26:07 +0330

    some-changes-in-order

--- resources/views/panel/orders/printable.blade.php
+++ resources/views/panel/orders/printable.blade.php
@@ -27,7 +27,7 @@
                                     <div class="invoice-logo">
                                         <!-- logo started -->
                                         <div class="logo">
-                                            <img src="/assets/images/header-logo.png"
+                                            <img src="/assets/images/img/logo-barman-red.png"
                                                  alt="logo">
                                         </div>
                                         <!-- logo ended -->
